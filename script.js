5e184dbc2383104071092e324481c91d0caf79b2
Tomas Holgado <creadordecaminos@gmail.com>
2023-01-14 11:48:43 -0300

    Created the HTML structure of the Quiz. Added some style to the app. Work in progress.

---
+++ script.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+        question: 'What atomic number symbolizes oxygen?',
+        a: '109',
+        b: '8',
+        c: '16',
+        d: '32',
+        answer: 'b'
+    }, {
+        question: '20 + 100 + 420 = ?',
+        a: '560',
+        b: '440',
+        c: '540',
+        d: '450',
+        answer: 'c'
+    }, {
+        question: 'In which year Argentina won the Football World Cup for the last time?',
+        a: '2014',
+        b: '2018',
+        c: '2016',
+        d: '2022',
+        answer: 'd'
+    }, {
+        question: "What is the name of the baby from The Simpson's family?",
+        a: 'Maggie',
+        b: 'Lisa',
+        c: 'Wanda',
+        d: 'Bart',
+        answer: 'a'
+    }, {
+        question: 'What colors does the Argentine flag have?',
+        a: 'Blue and White',
+        b: 'Sky blue, white and gold',
+        c: 'Sky blue and white',
+        d: 'None is correct',
+        answer: 'b'
+    }
+];
+
